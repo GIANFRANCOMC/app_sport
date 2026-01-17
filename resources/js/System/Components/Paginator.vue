3a992fbc5a6516011577cd398aeff05f4928482d
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-01-17 01:49:19 -0500

    All

--- resources/js/System/Components/Paginator.vue
+++ resources/js/System/Components/Paginator.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav aria-label="Page navigation" class="my-3" v-if="links.length > 0">
+    <nav aria-label="Page navigation" class="my-3" v-if="shouldShowPaginator">
         <ul class="pagination d-flex flex-wrap">
             <template v-for="(link, key) in links" :key="key">
                 <li :class="['page-item my-1', link.active ? 'active' : (link.url ? '' : 'disabled')]">
@@ -22,7 +22,15 @@ export default {
         }
     },
     computed: {
-        //
+        shouldShowPaginator() {
+
+            if(!this.links || this.links.length === 0) return false;
+
+            const pagesWithUrl = this.links.filter(link => link.url && !link.active);
+
+            return pagesWithUrl.length > 0;
+
+        }
     },
     methods: {
         handleClickPage({url}) {
