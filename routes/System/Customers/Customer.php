05344151241784f78c18879597b023bb970b01dd
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-01-18 00:05:24 -0500

    Customer - Backend - Routes

--- routes/System/Customers/Customer.php
+++ routes/System/Customers/Customer.php
@@ -5,13 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 $entity = "customers";
 
-Route::get('',            [CustomerController::class, 'index'])->name("$entity.index");
-Route::get('/initParams', [CustomerController::class, 'initParams'])->name("$entity.initParams");
-Route::get('/list',       [CustomerController::class, 'list'])->name("$entity.list");
-Route::get('/create',     [CustomerController::class, 'create'])->name("$entity.create");
-Route::post('',           [CustomerController::class, 'store'])->name("$entity.store");
-Route::get('/{id}/edit',  [CustomerController::class, 'edit'])->name("$entity.edit");
-Route::get('/{id}',       [CustomerController::class, 'show'])->name("$entity.show");
-Route::patch('/{id}',     [CustomerController::class, 'update'])->name("$entity.update");
-Route::get('/getSubscriptions/{id}', [CustomerController::class, 'getSubscriptions'])->name("$entity.getSubscriptions");
-Route::post('/registerBiometricFingerprint/{id}', [CustomerController::class, 'registerBiometricFingerprint'])->name("$entity.registerBiometricFingerprint");
+Route::get("",            [CustomerController::class, "index"])->name("$entity.index");
+Route::get("/initParams", [CustomerController::class, "initParams"])->name("$entity.initParams");
+Route::get("/list",       [CustomerController::class, "list"])->name("$entity.list");
+Route::get("/create",     [CustomerController::class, "create"])->name("$entity.create");
+Route::post("",           [CustomerController::class, "store"])->name("$entity.store");
+Route::get("/{id}/edit",  [CustomerController::class, "edit"])->name("$entity.edit");
+Route::get("/{id}",       [CustomerController::class, "show"])->name("$entity.show");
+Route::patch("/{id}",     [CustomerController::class, "update"])->name("$entity.update");
+Route::get("/getSubscriptions/{id}", [CustomerController::class, "getSubscriptions"])->name("$entity.getSubscriptions");
+Route::post("/registerBiometricFingerprint/{id}", [CustomerController::class, "registerBiometricFingerprint"])->name("$entity.registerBiometricFingerprint");
