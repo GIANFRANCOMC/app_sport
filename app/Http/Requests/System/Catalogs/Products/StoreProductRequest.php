9b825a26df376e9a5fe51c26392c12b4c2b690d9
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-02-02 16:29:15 -0500

    Catalogs - Backend - Form Request

--- app/Http/Requests/System/Catalogs/Products/StoreProductRequest.php
+++ app/Http/Requests/System/Catalogs/Products/StoreProductRequest.php
@@ -6,7 +6,7 @@ namespace App\Http\Requests\System\Catalogs\Products;
 
 use App\Helpers\System\Utilities;
 use Illuminate\Foundation\Http\FormRequest;
-use App\Rules\System\Defaults\UniqueInCompany;
+use App\Rules\System\Defaults\{UniqueInCompany};
 
 class StoreProductRequest extends FormRequest {
 
@@ -31,17 +31,12 @@ class StoreProductRequest extends FormRequest {
         $maxValue = Utilities::isDefined($this->max_price) && floatval($this->max_price) > 0 ? floatval($this->max_price) : Utilities::$inputs["maxValue"];
 
         $validations = [
-            "internal_code" => [
-                "required",
-                "string",
-                "max:100",
-                new UniqueInCompany("items", "internal_code", null, ["type" => "product"], "código interno")
-            ],
-            "name"          => "required|string|max:100",
-            "description"   => "nullable|string|max:300",
+            "internal_code" => ["required", "string", "max:50", new UniqueInCompany("items", "internal_code", null, ["type" => "product"], "código interno")],
+            "name"          => "required|string|max:50",
+            "description"   => "nullable|string|max:100",
             "price"         => "required|numeric|min:$minValue|max:$maxValue|decimal:0,$round",
             "currency_id"   => "required|integer",
-            "status"        => "required|string"
+            "status"        => "required|in:active,inactive"
         ];
 
         if(Utilities::isDefined($this->min_price) && floatval($this->min_price) > 0) {
--- app/Http/Requests/System/Catalogs/Subscriptions/StoreSubscriptionRequest.php
+++ app/Http/Requests/System/Catalogs/Subscriptions/StoreSubscriptionRequest.php
@@ -6,7 +6,7 @@ namespace App\Http\Requests\System\Catalogs\Subscriptions;
 
 use App\Helpers\System\Utilities;
 use Illuminate\Foundation\Http\FormRequest;
-use App\Rules\System\Defaults\UniqueInCompany;
+use App\Rules\System\Defaults\{UniqueInCompany};
 
 class StoreSubscriptionRequest extends FormRequest {
 
@@ -31,19 +31,14 @@ class StoreSubscriptionRequest extends FormRequest {
         $maxValue = Utilities::isDefined($this->max_price) && floatval($this->max_price) > 0 ? floatval($this->max_price) : Utilities::$inputs["maxValue"];
 
         $validations = [
-            "internal_code" => [
-                "required",
-                "string",
-                "max:100",
-                new UniqueInCompany("items", "internal_code", null, ["type" => "subscription"], "código interno")
-            ],
-            "name"           => "required|string|max:100",
-            "description"    => "nullable|string|max:300",
+            "internal_code"  => ["required", "string", "max:50", new UniqueInCompany("items", "internal_code", null, ["type" => "subscription"], "código interno")],
+            "name"           => "required|string|max:50",
+            "description"    => "nullable|string|max:100",
             "price"          => "required|numeric|min:$minValue|max:$maxValue|decimal:0,$round",
             "currency_id"    => "required|integer",
-            "duration_type"  => "required|string",
+            "duration_type"  => "required|in:hour,day,today,month,year",
             "duration_value" => "required|integer|min:1|max:$maxValue|decimal:0",
-            "status"         => "required|string"
+            "status"         => "required|in:active,inactive"
         ];
 
         if(Utilities::isDefined($this->min_price) && floatval($this->min_price) > 0) {
