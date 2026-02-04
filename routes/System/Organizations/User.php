b74cf4cfa058c5e40cd847ceb461b713466ce46a
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-02-04 14:49:40 -0500

    Category - Backend - Routes

--- routes/System/Organizations/User.php
+++ routes/System/Organizations/User.php
@@ -1,16 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\System\Organizations\{UserController};
 use Illuminate\Support\Facades\Route;
 
 $entity = "users";
 
-Route::get('',            [UserController::class, 'index'])->name("$entity.index");
-Route::get('/initParams', [UserController::class, 'initParams'])->name("$entity.initParams");
-Route::get('/list',       [UserController::class, 'list'])->name("$entity.list");
-Route::get('/create',     [UserController::class, 'create'])->name("$entity.create");
-Route::post('',           [UserController::class, 'store'])->name("$entity.store");
-Route::get('/{id}/edit',  [UserController::class, 'edit'])->name("$entity.edit");
-Route::get('/{id}',       [UserController::class, 'show'])->name("$entity.show");
-Route::patch('/{id}',     [UserController::class, 'update'])->name("$entity.update");
-
+Route::get("",            [UserController::class, "index"])->name("$entity.index");
+Route::get("/initParams", [UserController::class, "initParams"])->name("$entity.initParams");
+Route::get("/list",       [UserController::class, "list"])->name("$entity.list");
+Route::get("/create",     [UserController::class, "create"])->name("$entity.create");
+Route::post("",           [UserController::class, "store"])->name("$entity.store");
+Route::get("/{id}/edit",  [UserController::class, "edit"])->name("$entity.edit");
+Route::get("/{id}",       [UserController::class, "show"])->name("$entity.show");
+Route::patch("/{id}",     [UserController::class, "update"])->name("$entity.update");
+Route::delete("/{id}",    [UserController::class, "destroy"])->name("{$entity}.destroy");
