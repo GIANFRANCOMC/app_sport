a5df07854f45b91beedeeab06fee0e2c777184ed
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-02-04 16:16:13 -0500

    Category - Frontend- Main.js

--- resources/js/System/Pages/Organizations/users/main.js
+++ resources/js/System/Pages/Organizations/users/main.js
@@ -5,16 +5,18 @@ import App from "./main.vue"
 import VueSelect from "vue-select";
 import "vue-select/dist/vue-select.css";
 
-import Breadcrumb   from "@System/Components/Breadcrumb.vue";
-import InputDate    from "@System/Components/InputDate.vue";
-import InputNumber  from "@System/Components/InputNumber.vue";
-import InputSelect  from "@System/Components/InputSelect.vue";
-import InputSlot    from "@System/Components/InputSlot.vue";
-import InputSelect2 from "@System/Components/InputSelect2.vue";
-import InputText    from "@System/Components/InputText.vue";
-import Paginator    from "@System/Components/Paginator.vue";
-import Loader       from "@System/Components/Loader.vue";
-import WithoutData  from "@System/Components/WithoutData.vue";
+import Breadcrumb    from "@System/Components/Breadcrumb.vue";
+import InputDate     from "@System/Components/InputDate.vue";
+import InputNumber   from "@System/Components/InputNumber.vue";
+import InputSelect   from "@System/Components/InputSelect.vue";
+import InputSlot     from "@System/Components/InputSlot.vue";
+import InputSelect2  from "@System/Components/InputSelect2.vue";
+import InputText     from "@System/Components/InputText.vue";
+import InputTextArea from "@System/Components/InputTextArea.vue";
+import Paginator     from "@System/Components/Paginator.vue";
+import Loader        from "@System/Components/Loader.vue";
+import WithoutData   from "@System/Components/WithoutData.vue";
+import FiltersSection from "@System/Components/Generics/FiltersSection.vue";
 
 // App creation and mounted
 createApp(App)
@@ -26,7 +28,9 @@ createApp(App)
 .component("InputSlot", InputSlot)
 .component("InputSelect2", InputSelect2)
 .component("InputText", InputText)
+.component("InputTextArea", InputTextArea)
 .component("Paginator", Paginator)
 .component("Loader", Loader)
 .component("WithoutData", WithoutData)
+.component("FiltersSection", FiltersSection)
 .mount("#app");
