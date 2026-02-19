fb886342b3a5737d0307c77c16c918db7cc1df8b
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-02-19 18:03:01 -0500

    Asset - Backend - Routes

--- routes/System/Assets/Asset.php
+++ routes/System/Assets/Asset.php
@@ -1,15 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\System\Assets\{AssetController};
 use Illuminate\Support\Facades\Route;
 
 $entity = "assets";
 
-Route::get('',            [AssetController::class, 'index'])->name("$entity.index");
-Route::get('/initParams', [AssetController::class, 'initParams'])->name("$entity.initParams");
-Route::get('/list',       [AssetController::class, 'list'])->name("$entity.list");
-Route::get('/create',     [AssetController::class, 'create'])->name("$entity.create");
-Route::post('',           [AssetController::class, 'store'])->name("$entity.store");
-Route::get('/{id}/edit',  [AssetController::class, 'edit'])->name("$entity.edit");
-Route::get('/{id}',       [AssetController::class, 'show'])->name("$entity.show");
-Route::patch('/{id}',     [AssetController::class, 'update'])->name("$entity.update");
+Route::get("",            [AssetController::class, "index"])->name("$entity.index");
+Route::get("/initParams", [AssetController::class, "initParams"])->name("$entity.initParams");
+Route::get("/list",       [AssetController::class, "list"])->name("$entity.list");
+Route::get("/create",     [AssetController::class, "create"])->name("$entity.create");
+Route::post("",           [AssetController::class, "store"])->name("$entity.store");
+Route::get("/{id}/edit",  [AssetController::class, "edit"])->name("$entity.edit");
+Route::get("/{id}",       [AssetController::class, "show"])->name("$entity.show");
+Route::patch("/{id}",     [AssetController::class, "update"])->name("$entity.update");
+Route::delete("/{id}",    [AssetController::class, "destroy"])->name("{$entity}.destroy");
