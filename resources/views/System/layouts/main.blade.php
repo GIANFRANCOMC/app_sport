c3b8cc25d5594f49dad1034cb86b0f3dfe2c830f
GIANFRANCOMC <64445493+GIANFRANCOMC@users.noreply.github.com>
2026-04-05 00:43:21 -0500

    Update br-branding.css

--- resources/views/System/layouts/main.blade.php
+++ resources/views/System/layouts/main.blade.php
@@ -271,7 +271,7 @@
         <div class="br-fab-favorites" id="brFabFavorites" data-open="0">
             <div class="br-fab-favorites__panel" id="brFabFavoritesPanel" role="region" aria-labelledby="brFabFavoritesTitle" aria-hidden="true">
                 <div class="br-fab-favorites__head">
-                    <span id="brFabFavoritesTitle" class="br-fab-favorites__title ms-2">Favoritos</span>
+                    <span id="brFabFavoritesTitle" class="br-fab-favorites__title">Favoritos</span>
                     <button type="button" class="br-fab-favorites__close" id="brFabFavoritesClose" aria-label="Cerrar panel de favoritos">
                         <i class="fa-solid fa-xmark" aria-hidden="true"></i>
                     </button>
